96fb1b662ad7043ae3d56c93adec6b3d6ea41083
zfehrn-bbc <nico.fehr@gmx.ch>
2015-06-17 16:16:27 +0200

    Revert "Revert "changes""
    This reverts commit ba6b050bef18087313d5ef2503915a30abbc880c.

--- app/src/main/java/ch/berufsbildungscenter/train_alert/MainActivity.java
+++ app/src/main/java/ch/berufsbildungscenter/train_alert/MainActivity.java
@@ -1,9 +1,12 @@
 package ch.berufsbildungscenter.train_alert;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent intent = new Intent(this, VerbindungenActivity.class);
+        intent.putExtra("hi", 6);
+
+        Button button = (Button) findViewById(R
+                .id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
